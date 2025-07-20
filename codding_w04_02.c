5b3cf972b629ad253d32644430e0e3320fb9919f
jittrawanpadsang <s6812022611201@email.kmunt.ac.th>
2025-07-20 14:30:40 +0700

    02

---
+++ codding_w04_02.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // includeไม่มี # กำกับ และ stdio.h ไม่มี <>
+
+int main() {
+    int a = 10; // ไม่มี semicolon
+    printf("a = %d", a); //ไม่มี semicolon
+    return 0; //ไม่มี semicolon
+}
